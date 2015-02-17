996e33dff4dbad01fe02e03d01feb62256ac0bb9
Bryan Reilly <94bryanr@gmail.com>
2015-02-17 10:08:49 -0700

    Control flow thorugh PGMreader

--- src/reader/PGMreader.cpp
+++ src/reader/PGMreader.cpp
@@ -8,65 +8,11 @@ using std::endl;
 using std::ifstream;
 using std::cerr;
 
-PGMreader::PGMreader(string filename){ 
-	fileStream.open(filename.c_str(), std::ios_base::in);
-	if(!fileStream){
-		exitWithError("File can not be opened");
-	}
-
-	testMagicNumber();
-	readFile();
+PGMreader::PGMreader(string filename)
+	: reader(filename) {
 }
 
 PGM PGMreader::getPGM(){
-    //Find beginning and end of pixel data
-	vector<int>::const_iterator first = fileData.begin() + 3;
-	vector<int>::const_iterator last = fileData.end();
-
-	vector<unsigned int> pixelData(first, last);
-
-	PGM pgm(fileData[0], fileData[1], fileData[2], pixelData);
-	return pgm;
-}
-
-
-void PGMreader::testMagicNumber(){
-	char firstChar;
-	char secondChar;
-	fileStream.get(firstChar);
-	fileStream.get(secondChar);
-	bool correctMagicNumbers = (firstChar == 'P' && secondChar == '2');
-	if(!correctMagicNumbers){
-		exitWithError("File does not have magic numbers!");
-	}
-}
-
-void PGMreader::readFile(){
-	int current;
-	while(!fileStream.eof()){
-		if(fileStream >> current){
-			//Current is an integer
-			fileData.push_back(current);
-		}
-		else if(fileStream.fail() && !fileStream.eof()){
-			//We are not reading a digit
-			fileStream.clear(); //Clear the fail bit
-			string comment;
-			if(std::getline(fileStream, comment)){
-				//We have read in a comment
-				//Check for comment delimiter 
-				if(comment[0] != '#'){
-					exitWithError("Incorrect comment formatting");
-				}
-			}
-			else{
-				exitWithError("Unknown input value");
-			}
-		}
-	}
-}
-
-void PGMreader::exitWithError(string errorMessage){
-	cerr << "Error: " << errorMessage << endl;
-	exit(-1);
+	PGM outputPGM = reader.getPGM();
+	return outputPGM;
 }
--- src/reader/PGMreader.h
+++ src/reader/PGMreader.h
@@ -6,6 +6,7 @@
 	#define USING_PGM
 	#include "../PGM.h"
 #endif
+#include "PGMAreader.h"
 
 using std::fstream;
 using std::string;
@@ -19,14 +20,6 @@ class PGMreader{
 		PGM getPGM();
 
 	private:
-		/// File stream, opens file passed in to constructor.
-		fstream fileStream;
-		/// Vector of ALL file data, including width, height, max.
-		vector<int> fileData;
-		/// Check to see if 'P2' are the first characters of the file.
-		void testMagicNumber();
-		/// Opens the filestream and constructs the fileData vector.
-		void readFile();
-		/// Exits the program by returning -1 to the system and outputting an error to cerr.
-		void exitWithError(string errorMessage);
+		/// Holds the PGM reader
+		PGMAreader reader;
 };
